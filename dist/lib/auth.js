b8a9d87cc6c2172347c07dacd8c94350ac256cc3
Oliver Boyd <flashdrive284@gmail.com>
2022-08-05 21:26:29 +1000

    gen numeric id v2

--- dist/lib/auth.js
+++ dist/lib/auth.js
@@ -54,8 +54,7 @@ function generateId(type = "hex") {
         return crypto.randomBytes(26).toString("hex");
     else if (type == "num") {
         // hi-tech numeric id generation right here
-        const time = Date.now() + Math.floor(Math.random() * 10);
-        return parseInt(time.toString(8));
+        return parseInt(Date.now().toString(8) + Math.floor(Math.random() * 10));
     }
     return;
 }
